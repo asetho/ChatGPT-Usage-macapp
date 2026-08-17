85b89b396bf848c7c9c7aa817fea66d71cee9d95
Aaron <208292680+aaronwhippo@users.noreply.github.com>
2026-08-17 12:58:23 -0400

    Improve Windows tray icon clarity

--- Windows/TrayIcon.cs
+++ Windows/TrayIcon.cs
@@ -1,5 +1,7 @@
 using Drawing = System.Drawing;
+using Drawing2D = System.Drawing.Drawing2D;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace ChatGPTUsage.Windows;
 
@@ -14,7 +16,31 @@ internal static class TrayIcon
         }
 
         using var appIcon = new Drawing.Icon(appIconPath, 32, 32);
+        using var source = appIcon.ToBitmap();
+        using var trayBitmap = new Drawing.Bitmap(32, 32, Drawing.Imaging.PixelFormat.Format32bppArgb);
+        using var graphics = Drawing.Graphics.FromImage(trayBitmap);
+        using var clip = new Drawing2D.GraphicsPath();
 
-        return (Drawing.Icon)appIcon.Clone();
+        graphics.Clear(Drawing.Color.Transparent);
+        graphics.InterpolationMode = Drawing2D.InterpolationMode.HighQualityBicubic;
+        graphics.PixelOffsetMode = Drawing2D.PixelOffsetMode.HighQuality;
+        graphics.CompositingQuality = Drawing2D.CompositingQuality.HighQuality;
+        clip.AddEllipse(0, 0, 32, 32);
+        graphics.SetClip(clip);
+        graphics.DrawImage(source, new Drawing.Rectangle(1, 1, 30, 30), 4, 4, 24, 24, Drawing.GraphicsUnit.Pixel);
+
+        var handle = trayBitmap.GetHicon();
+        try
+        {
+            using var nativeIcon = Drawing.Icon.FromHandle(handle);
+            return (Drawing.Icon)nativeIcon.Clone();
+        }
+        finally
+        {
+            DestroyIcon(handle);
+        }
     }
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern bool DestroyIcon(nint hIcon);
 }
